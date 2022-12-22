b84fc5bfc7e253af39997a0f91fc740c1358ede6
Bigsheun <bigsheun1812@gmail.com>
2022-12-22 05:54:53 +0100

    'another-mmm'

--- external_cmd.c
+++ external_cmd.c
@@ -11,7 +11,8 @@ int perform_external_cmd(info_t *info, char **envp)
 	}
 	if (child_pid == 0)
 	{
-		execve(info->cmd_buf[0], info->cmd_buf, envp);
+		execve(get_absolute_path(info, info->cmd_buf[0]),
+		info->cmd_buf, envp);
 
 		/* TODO: PUT ERROR FUNCTION */
 	}
